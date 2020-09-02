e79e9521b3aee4febeef51a34e5bd05c7d556566
Kendyl Hull <hullkendyl@gmail.com>
2020-09-02 10:18:33 -0400

    added @GetMappings to SpeciesController

--- backend-api/src/main/java/com/animalshelter/controllers/SpeciesController.java
+++ backend-api/src/main/java/com/animalshelter/controllers/SpeciesController.java
@@ -1,7 +1,14 @@
 package com.animalshelter.controllers;
 
+import com.animalshelter.entities.Species;
 import com.animalshelter.storages.SpeciesStorage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
+@RestController
 public class SpeciesController {
 
     private SpeciesStorage speciesStorage;
@@ -9,4 +16,11 @@ public class SpeciesController {
     public SpeciesController(SpeciesStorage speciesStorage) {
         this.speciesStorage = speciesStorage;
     }
+
+    @GetMapping("/api/species")
+    public Collection<Species> retrieveAllSpecies() { return speciesStorage.retrieveAllSpecies(); }
+
+    @GetMapping("/api/species/{id}/")
+    public Species retrieveSpeciesById(@PathVariable long id) { return speciesStorage.retrieveSpeciesById(id); }
+    
 }
