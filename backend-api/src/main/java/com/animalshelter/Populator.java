5cf486ebfa338c6289d361b6da06df43b95e2d0b
Kendyl Hull <hullkendyl@gmail.com>
2020-09-15 11:25:34 -0400

    added more species to the populator

--- backend-api/src/main/java/com/animalshelter/Populator.java
+++ backend-api/src/main/java/com/animalshelter/Populator.java
@@ -15,8 +15,17 @@ public class Populator implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Species species1 = new Species("Cat", "","/images/kitten-flowers.jpg");
+        Species species2 = new Species("Dog", "", "/images/dog.jpg");
+        Species species3 = new Species("Rabbit", "", "/images/rabbit.jpg");
+        Species species4 = new Species("Ferret", "", "/images/ferret.jpg");
+        Species species5 = new Species("Guinea Pig", "", "/images/guinea-pig.jpg");
+
 
         speciesStorage.save(species1);
+        speciesStorage.save(species2);
+        speciesStorage.save(species3);
+        speciesStorage.save(species4);
+        speciesStorage.save(species5);
     }
 
 }
